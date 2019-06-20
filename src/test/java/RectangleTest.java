10e2db759c3d426d9eca71e19c36037c79e32745
qlyan(benben) <qlyan@thoughtworks.com>
2019-06-20 19:21:10 +0800

    add rectangle

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -3,8 +3,14 @@ import org.junit.Test;
 
 public class RectangleTest {
     @Test
-    public void should_get_area_for_rectangle() {
+    public void should_get_area_as_2_for_rectangle_with_1_width_and_2_height() {
         Rectangle rectangle = new Rectangle(1, 2);
         Assert.assertEquals(rectangle.getArea(), 2);
+    }
+
+    @Test
+    public void should_get_perimeter_as_4_for_rectangle_with_1_width_and_2_height() {
+        Rectangle rectangle = new Rectangle(1, 2);
+        Assert.assertEquals(rectangle.getPerimeter(), 6);
     }
 }
