d72c453896d6cf39473fdf3f28c6e6a58683d1c0
qlyan(benben) <qlyan@thoughtworks.com>
2019-06-20 20:04:55 +0800

    add given and then

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -4,13 +4,17 @@ import org.junit.Test;
 public class RectangleTest {
     @Test
     public void should_get_area_as_2_for_rectangle_with_1_width_and_2_height() {
+        // given
         Rectangle rectangle = new Rectangle(1, 2);
+        // then
         Assert.assertEquals(rectangle.getArea(), 2);
     }
 
     @Test
     public void should_get_perimeter_as_4_for_rectangle_with_1_width_and_2_height() {
+        // given
         Rectangle rectangle = new Rectangle(1, 2);
+        // then
         Assert.assertEquals(rectangle.getPerimeter(), 6);
     }
 }
